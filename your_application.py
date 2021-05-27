e89874e1091d3bdc5839460aaefcd17cdfed9102
bennyantony2500 <51130310+bennyantony2500@users.noreply.github.com>
2021-05-27 09:37:12 +0530

    Create your_application.py

---
+++ your_application.py
@@ -0,0 +1,47 @@
+
+# Important Plugins
+import streamlit as st #start off by loading streamlit
+import nltk
+nltk.download('punkt')
+
+# importing Newspaper3k
+import newspaper
+from newspaper import Article
+
+# Custom Functions
+#---------------------------------
+
+#article downloader
+def run_api(user_url_input):
+    article = Article(user_url_input)
+    article.download()
+    article.parse()
+    article.nlp()
+    return meta_extraction(article)
+
+#article meta extraction + summarizer
+def meta_extraction(article):
+    st.write('**The title of this article is ** ' + article.title)
+    st.write('**The author of this article is **' + str(article.authors))
+    st.write('**The published date of this article is **' + str(article.publish_date))
+    st.write('**The source of this article is **' + article.source_url)
+
+    st.write('## Top Keywords of this Article Below:')
+    st.write(str(article.keywords))
+    st.write('## Summary of Article Below:')
+    st.write(article.summary)
+    return
+
+
+# Page Design
+#-------------------------------
+st.markdown('# This Streamlit application will scrape, display some meta data, and summarize articles.')
+
+# Text Input
+user_url_input = st.text_input("Please enter the URL of article")
+
+# Button to Run the script
+if st.button('Summarize the Article'):
+    st.write(run_api(user_url_input))
+else:
+    st.write(st.markdown('**Button not yet executed.**'))
